02e96f27b4a4b4dff54e4069a871cc586ef9e191
adventure-capitalist <ashleylaurel@outlook.com>
2020-03-29 21:34:55 +0100

    improving the log

--- src/components/log.jsx
+++ src/components/log.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
+import { findByLabelText } from "@testing-library/react";
 class Log extends Component {
+  state = {
+    adding: false,
+  }
+
+  clickHandler = () => {
+    let status = this.state.adding ? false : true
+    this.setState({adding: status});
+  }
   render() {
     return (
       <div style={{ height: "85vh" }}>
@@ -7,9 +16,18 @@ class Log extends Component {
           <h2>{this.props.cycleLength}</h2>
           <p>days in your average cycle</p>
         </div>
+      
         <div className="logBook">
-          <button>Add an entry</button>
-        </div>
+          <button onClick={this.clickHandler}>Add an entry</button>
+        </div>{this.state.adding ? 
+        (<form style={{margin: "auto", width: "30vw", display: "flex", flexDirection: "column"}}>
+          <label>Log</label>
+          <input type="text"style={{height: "20vh"}}></input>
+          <label>Date</label>
+          <input type="date" />
+          <button style={{marginTop: "10px"}}>Submit</button>
+        </form>)
+        : null}
       </div>
     );
   }
