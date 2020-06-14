7cdd92e5d86f70c53fb4216bad17b01c657416b2
adventure-capitalist <ashleylaurel@outlook.com>
2020-06-14 10:48:52 +0100

    adding the new sql file

--- moon-tracker-backend/moon-keeper.sql
+++ moon-tracker-backend/moon-keeper.sql
@@ -24,11 +24,25 @@ DROP TABLE IF EXISTS `cycles`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cycles` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `start` varchar(45) NOT NULL,
-  `end` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `title` varchar(45) DEFAULT 'Menstruation',
+  `start` date NOT NULL,
+  `end` date NOT NULL,
+  `allDay` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `start_UNIQUE` (`start`),
+  UNIQUE KEY `end_UNIQUE` (`end`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cycles`
+--
+
+LOCK TABLES `cycles` WRITE;
+/*!40000 ALTER TABLE `cycles` DISABLE KEYS */;
+INSERT INTO `cycles` VALUES (1,'Menstruation','2020-05-08','2020-05-15',1),(2,'Menstruation','2020-06-10','2020-06-17',1);
+/*!40000 ALTER TABLE `cycles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tracker`
@@ -39,11 +53,25 @@ DROP TABLE IF EXISTS `tracker`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tracker` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `date` date DEFAULT NULL,
-  `note` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `start` date NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `end` date DEFAULT NULL,
+  `allDay` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `desc_UNIQUE` (`title`),
+  UNIQUE KEY `start_UNIQUE` (`start`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tracker`
+--
+
+LOCK TABLES `tracker` WRITE;
+/*!40000 ALTER TABLE `tracker` DISABLE KEYS */;
+INSERT INTO `tracker` VALUES (1,'2020-06-18','flow: 4, pain: 2, location: Legs, note: I have a tummy ache','2020-06-18',1),(2,'2020-06-13','flow: 3, pain: 4, location: Legs, note: I have a tummy ache','2020-06-13',1);
+/*!40000 ALTER TABLE `tracker` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -54,4 +82,4 @@ CREATE TABLE `tracker` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-08 17:15:27
+-- Dump completed on 2020-06-13 16:16:15
